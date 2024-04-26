21f5b3a732bfa8d84d9cf899fe10e4b90d4c0456
arthur <arthurpego1@gmail.com>
2024-04-26 09:26:48 -0300

    estilizando e criando o javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+let p1=prompt("digite seu nome")
+let p2=prompt("digite seu poder")
+let p3=prompt("digite um nome de um vilao")
+let p4=prompt("digit um lugar")
+let msg=document.getElementById("msg")
+
+msg.innerHTML=`ola ${p1}, o seu poder é ${p2}, e voce ira enfrentar o(a) ${p3}, no(a)(em) ${p4}`
